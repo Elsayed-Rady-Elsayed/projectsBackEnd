a3d24c1283aa65ecc3cd7e3ab767ca7ed6a5dc0b
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2025-01-25 01:59:38 +0200

    edit

--- index.js
+++ index.js
@@ -1,77 +1,79 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = new express();
 
-const projects = require("./modules/project")
+const app = express();
+const projects = require("./modules/project");
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    app.post("/project",async(req,res)=>{
-        try{
-            const project = new projects();
-            const body = req.body;
-            project.mainImage = body.mainImage;
-            project.title = body.title;
-            project.desc = body.desc;
-            await project.save();
-            res.send(project);
-            return;
-        }catch(e){
-            res.send(e.message);
-            return;
-        }
-    });
-    app.delete("/project/:id",async(req,res)=>{
-        try{
-            await projects.findByIdAndDelete(req.params.id);
-            res.send("removed");
-            return;
-        }catch(e){
-            res.send(e.message);
-            return;
-        }
-    });
-    app.get("/project",async(req,res)=>{
-        try{
-            const projectsAll = await projects.find();
-            res.json(projectsAll);
-            return;
-        }catch(e){
-            res.send(e.message);
-            return;
-        }
-    });
-    app.get("/project/:id",async(req,res)=>{
-        try{
-            const project = await projects.findById(req.params.id);
-            res.json(project);
-            return;
-        }catch(e){
-            res.send(e.message);
-            return;
-        }
-    });
-    app.put("/project/:id",async(req,res)=>{
-        const updateDate = {
-            title:req.body.title,
-            mainImage:req.body.mainImage,
-            desc:req.body.desc
-        };
-        try{
-            const updateProject = await projects.findByIdAndUpdate(req.params.id,updateDate,{new:true})
-            res.send(updateProject);
-            return;
-        }catch(e){
-            res.send(e.message);
-            return;
-        }
-    });
-}).catch((e)=>{
-    console.log(e);
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the API!");
 });
 
+app.post("/project", async (req, res) => {
+    try {
+        const project = new projects();
+        const body = req.body;
+        project.mainImage = body.mainImage;
+        project.title = body.title;
+        project.desc = body.desc;
+        await project.save();
+        res.send(project);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
 
-// app.listen(3000,()=>{
-//     console.log("the app is running in port 3000");
-// })
+app.delete("/project/:id", async (req, res) => {
+    try {
+        await projects.findByIdAndDelete(req.params.id);
+        res.send("removed");
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+app.get("/project", async (req, res) => {
+    try {
+        const projectsAll = await projects.find();
+        res.json(projectsAll);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+app.get("/project/:id", async (req, res) => {
+    try {
+        const project = await projects.findById(req.params.id);
+        res.json(project);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+app.put("/project/:id", async (req, res) => {
+    const updateData = {
+        title: req.body.title,
+        mainImage: req.body.mainImage,
+        desc: req.body.desc,
+    };
+    try {
+        const updatedProject = await projects.findByIdAndUpdate(
+            req.params.id,
+            updateData,
+            { new: true }
+        );
+        res.send(updatedProject);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
 module.exports = app;
